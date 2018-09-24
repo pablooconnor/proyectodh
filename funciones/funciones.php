509b19c03ce884f68527628ce5fdacdb2501fd2e
rheredia <totti.heredia@gmail.com>
2018-09-24 07:11:31 -0300

    Formulario del feed en proceso

--- funciones/funciones.php
+++ funciones/funciones.php
@@ -229,3 +229,74 @@ function logout() // no use esta funcion
     redirect('index.php');
 
 }
+
+/* <<<<<<<<<<<<  FEED  >>>>>>>>>>>>> */
+
+function feedPopulate() {
+
+}
+
+function feedConnect()
+{
+    $db = file_get_contents('posts.json');
+    $arr = explode(PHP_EOL, $db);
+
+    foreach($arr as $post) {
+        $postArray[] = json_decode($post, true);
+    }
+
+    return $postArray;
+}
+
+function validatePost($post){
+    $errors = [];
+    if($post['title'] == ""){
+        $errors['title'] = "No puedes dejar el titulo vacío.";
+    }
+    if($post['description'] == ""){
+        $errors['description'] = "No puedes dejar la descripción vacía.";
+    }
+    if(!issed($post['provincia'])){
+        $errors['provincia'] = "Debes escoger una provincia.";
+    }
+
+    return $errors;
+}
+
+function createPost($post){
+
+}
+
+function postSave($post)
+{
+    $jsonPost = json_encode($post);
+    file_put_contents('post.json', $jsonUser . PHP_EOL, FILE_APPEND);
+}
+
+function validatePostAvatar($data) 
+{
+    $errores = [];
+
+    $username = $data["username"];
+
+    if($_FILES["avatar"]["error"] == UPLOAD_ERR_OK) {
+        $nombre = $_FILES["avatar"]["name"];
+        $archivo = $_FILES["avatar"]["tmp_name"];
+        
+        $ext = pathinfo($nombre, PATHINFO_EXTENSION);
+
+        if($ext != "jpg" && $ext != "png" && $ext != "jpeg") {
+            $errores["avatar"] = "Solo acepto formatos jpg y png";
+            return $errores;
+        }
+
+        $miArchivo = dirname(__FILE__);
+        $miArchivo = $miArchivo . "/img/";
+        $miArchivo = $miArchivo. "perfil" . $username . "." . $ext;
+        move_uploaded_file($archivo, $miArchivo);
+
+    } else {
+        $errores["avatar"] = "Hubo un error al procesar el archivo";
+    }
+    return $errores;
+}
--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
     require('funciones/funciones.php');
+
+    if($_POST){
+        //Chequear de que formulario es (proximamente)
+        $errors = validatePost($_POST);
+        $post = createUser($_POST);
+    // Si hay archivos, y en $_FILES la key 'error' esta seteada en 0... (para visualizar esto, hacer un dd() de $_FILES a ver que llega y COMO)
+    if(empty($_FILES['avatar']['error']) == true) {
+        //procesa con validateAvatar()
+        $avatarErrors = validateAvatar($_POST);
+        //y creame una nueva key en el array $usuario llamada 'avatar' a la cual le asignamos el value de lo que devuelve la funcion photoPath()
+        $usuario['avatar'] = photoPath($_POST);
+        //SI no esta vacio el array $avatarErrors...
+        if(!empty($avatarErrors)) {
+            //mergea con array_merge() todos los errores en un solo array $errors
+            $errors = array_merge($errors, $avatarErrors); 
+        }
+    }
+    //si count() de $errors es == a 0
+    if(count($errors) == 0) {  
+        //guarda el usuario en Json con saveUser()
+        saveUser($usuario);
+        //y redirigime a Login (NUNCA directo al perfil)
+        redirect('login.php');
+    }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +41,34 @@
     <div class="main">
         <?php include_once('header.php'); ?>
         <main>
-
+            <h2>Crear nueva busqueda o reportar mascota encontrada</h2>
+            <form class="createPost"action="" enctype="multipart/form-data" method="post">
+                <div class="createTexts">
+                    <input type="text" name="title" placeholder="Escribe un título...">
+                    <textarea class="textarea" name="description" id="" cols="30" rows="5" placeholder="Escribe una breve descripcion (Max. 250 caracteres)" maxlength="250"></textarea>
+                </div>
+                <div class="createButtons">
+                    <input class="file" type="file" name="foto">
+                    <div class="radioButtons">
+                        <div>
+                            <input type="radio" name="tipo" value="found" id="found" required>
+                            <label for="found">Encontré una mascota.</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="tipo" value="search" id="search">
+                            <label for="found">Busco una mascota.</label>
+                        </div>
+                    </div>
+                    <select class="selectPlace" name="provincia" id="provincia">
+                        <option value="" selected hidden disabled>Seleccione la zona</option>
+                        <option value="caba">C.A.B.A.</option>
+                        <option value="gbanorte">G.B.A. Norte</option>
+                        <option value="gbasur">G.B.A. Sur</option>
+                        <option value="gbaoeste">G.B.A. Oeste</option>
+                    </select>
+                    <input class="submit" type="submit" value="Enviar">
+                </div>
+            </form>
         </main>
         <?php include_once('footer.php'); ?>
     </div>
