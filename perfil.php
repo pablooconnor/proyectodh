d3f7e02eb3cf35a8b4826ec99c59b813f561cb82
rheredia <totti.heredia@gmail.com>
2018-09-25 00:07:00 -0300

    Perfil style

--- perfil.php
+++ perfil.php
@@ -33,7 +33,7 @@
     <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
     <link rel="stylesheet" href="css/perfil.css">
     <link rel="stylesheet" href="css/header-footer.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 </head>
 <body>
     <div class="main">
@@ -47,50 +47,30 @@
                 </div>
             <?php else: ?>
                 <div class="container">
-                    <div class="row">
-                        <div class=" col-lg-offset-3 col-lg-6">
-                            <div class="panel panel-default">
-                                <div class="panel-body">
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="row">
-                                                <div class="col-sm-offset-3 col-sm-6 col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6">
-                                                    <?php //si NO TIENE AVATAR ?>
-                                                    <?php if(!isset($user['avatar'])):?>
-                                                    <?php //si Cargame la imagen de d10s ?>
-                                                    <img class="img-circle img-responsive" src="img/default.png" alt="avatar default">
-                                                    <?php else: ?>
-                                                    <?php // ELSE -----> cargame su avatar ?>
-                                                    <img class="img-circle img-responsive" src="img/<?=$avatar?>" alt="avatar">
-                                                    <?php endif;?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="row">
-                                                <div class="centered-text col-sm-offset-3 col-sm-6 col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6">
-                                                    <div>
-                                                        <h3><?="Bienvenido $username!" ?></h3>
-                                                        <p> <?="Email: $email" ?></p>
-                                                        <p><?="Provincia: $provincia" ?></p>
-                                                        <p> <?="Direccion: $direction" ?></p>
-                                                    </div>
-                                                    <div class="button-container">
-                                                        <button type="submit"><a href="editarperfil.php">Editar</button>
-                                                        <button type="reset" ><a href="logout.php">Logout</a></button>
-                                                        <?php if(checkRole($_SESSION['email']) == true): ?>
-                                                        <button type="reset"><a class="nav-link" href="backend.php">Administrar</a></button>
-                                                        <?php endif; ?>
-                                                        <?php // mail: admin@gmail.com password:123456 roll:7 ?>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="profile">
+                        <div class="avatar">
+                            <?php //si NO TIENE AVATAR ?>
+                            <?php if(!isset($user['avatar'])):?>
+                            <?php //si Cargame la imagen de d10s ?>
+                            <img class="img-circle img-responsive" src="img/default.png" alt="avatar default">
+                            <?php else: ?>
+                            <?php // ELSE -----> cargame su avatar ?>
+                            <img class="img-circle img-responsive" src="img/<?=$avatar?>" alt="avatar">
+                            <?php endif;?>
+                        </div>
+                        <div class="information">
+                            <h3><?="Bienvenido $username!" ?></h3>
+                            <p> <?="Email: $email" ?></p>
+                            <p><?="Provincia: $provincia" ?></p>
+                            <p> <?="Direccion: $direction" ?></p>
+                        </div>
+                        <div class="button-container">
+                            <button type="submit"><a href="editarperfil.php">Editar</button>
+                            <button type="submit" ><a href="logout.php">Logout</a></button>
+                            <?php if(checkRole($_SESSION['email']) == true): ?>
+                            <button type=""><a class="nav-link" href="backend.php">Administrar</a></button>
+                            <?php endif; ?>
+                            <?php // mail: admin@gmail.com password:123456 roll:7 ?>
                         </div>
                     </div>
                 </div>
