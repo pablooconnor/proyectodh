00111dc169c4d3186baf8efe84c46494b53b0e76
gustavobargas <42453722+gustavobargas@users.noreply.github.com>
2018-10-12 10:52:47 -0300

    Add files via upload

---
+++ login + POO.php
@@ -0,0 +1,72 @@
+<?php
+//require 'funciones.php';
+require 'loader.php';
+
+if(Auth::check()) {
+    redirect('perfil.php');
+}
+
+if($_POST) {
+    $usuarioArray = $db->emailDbSearch($_POST['email']);
+    $user = new User($usuarioArray['username'], $usuarioArray['email'], $usuarioArray['password'], $usuarioArray['role']);
+    $arrayErr = [];
+
+    if($usuarioArray !== null) {
+        $error = "Nombre de usuario o pass incorrectos";
+        !Validate::loginValidate($_POST['password'], $user) ? $arrayErr['login'] = $error : Auth::login($user);
+        redirect('perfil.php');
+
+    }
+}
+        
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/header-footer.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+</head>
+<body>
+    <div class="main">
+        <?php include_once('header.php')?>
+        <main>
+        <form class="form" action="" method="post">
+            <h2>Inicia sesion</h2>
+            
+                <div class="form-flex">
+                  <label class="lable" ><b>Email</b></label>
+                  <input class="textfield"  placeholder="Ingresar Usuario" name="email" required>
+              
+                  <label class="lable"><b>Contraseña</b></label>
+                  <input class="textfield" type="password" placeholder="Ingresar Contraseña" name="password" required>
+
+                  <div class="button-container">
+                  <button type="submit">Iniciar sesión</button>
+                  <label>
+
+                  <button type="button" class="cancelbtn">Cancelar</button>
+                </div>
+
+                  <span class="psw">¿Olvidaste tu <a href="#">contraseña?</a></span>
+
+                <div class="col">
+                    <a href="#" class="fb btn">
+                      <i class="fa fa-facebook"></i> Iniciar con Facebook
+                    </a>
+                    <a href="#" class="google btn">
+                      <i class="fa fa-google"></i> Iniciar con Google+
+                    </a>
+                  </div>            
+              </form>
+            </main>
+            <?php include_once('footer.php')?>
+      </div>
+</body>
+</html>
